52cea9e4266e6a6080668ea274cbf5e06b5992dd
hasip_timurtas <hasip.timurtas@gmail.com>
2019-10-22 21:19:28 +0300

    refactoring

--- src/Reel.ts
+++ src/Reel.ts
@@ -3,27 +3,27 @@ import * as TWEEN from '@tweenjs/tween.js';
 export class Reel {
     public container!: PIXI.Sprite;
     public reelIdx: number = 0;
-    private val!: number;
+    private val: number;
     private oldVal!: number;
     private readonly reelTextures: string[] = ['A', 'J', 'K', 'Q', 'P','T'];
-    private readonly units: PIXI.Sprite[] = [];
+    private readonly cards: PIXI.Sprite[] = [];
     private readonly scale: number = 0.5;
     private readonly reelRadius: number = 190;
     private readonly reelCenter: PIXI.Point = new PIXI.Point(0, 0);
 
     constructor() {
         this.init();
-        this.arrangeUnits(0);
+        this.arrangeCards(0);
         this.value = this.getRandVal();
         this.changeFront();
     }
 
-    public get width(): number {
-        return this.units[2].width * this.scale;
-    }
-
     public set value(v: number) {
         this.val = v;
+    }
+
+    public get width(): number {
+        return this.cards[2].width * this.scale;
     }
 
     public changeValue(v: number): void {
@@ -39,25 +39,26 @@ export class Reel {
     }
 
     private init(): void {
+        // Create card sprites 
         this.container = new PIXI.Sprite();
         this.reelTextures.forEach((img: string) => {
-            const unit: PIXI.Sprite = new  PIXI.Sprite()
-            unit.texture =  PIXI.Texture.from(`assets/${img}.png`);
-            unit.pivot.set(unit.width * 0.5, unit.height * 0.5);
-            unit.height = 180
-            this.units.push(unit);
-            this.container.addChild(unit);
+            const card: PIXI.Sprite = new  PIXI.Sprite()
+            card.texture =  PIXI.Texture.from(`assets/${img}.png`);
+            card.pivot.set(card.width * 0.5, card.height * 0.5);
+            card.height = 180
+            this.cards.push(card);
+            this.container.addChild(card);
         });
         this.container.scale.set(this.scale);
         this.container.anchor.set(0.5, 0.5);
     }
 
-    private arrangeUnits(spinnedDegree: number): void {
-        const unitsNumber: number = this.units.length;
+    private arrangeCards(spinnedDegree: number): void {
+        const unitsNumber: number = this.cards.length;
         const radianShare: number = (360 * (Math.PI / 180)) / unitsNumber;
         const spinnedRad: number = spinnedDegree * (Math.PI / 180);
 
-        this.units.forEach((unit: PIXI.Sprite, index: number) => {
+        this.cards.forEach((unit: PIXI.Sprite, index: number) => {
             const rad: number = radianShare * index + spinnedRad;
             const positionX: number = this.reelCenter.x + this.reelRadius * Math.sin(rad);
             const positionY: number = this.reelCenter.y + this.reelRadius * Math.cos(rad);
@@ -68,12 +69,11 @@ export class Reel {
 
     private changeFront(): void {
         const degPerUnits: number[] = [90, -30, -150];
-        this.arrangeUnits(degPerUnits[this.val]);
+        this.arrangeCards(degPerUnits[this.val]);
     }
 
     private changeFrontWithSpin(): void {
         const degPerUnits: number[] = [90, -30, -150];
-        const unitsNumber: number = this.units.length;
         const defaultSpinDeg: number = -10 * 360;
         const targetSpinDeg: number = defaultSpinDeg + degPerUnits[this.val];
         const startDeg: number = degPerUnits[this.oldVal];
@@ -82,7 +82,7 @@ export class Reel {
             .easing(TWEEN.Easing.Exponential.InOut)
             .delay(this.reelIdx * 200)
             .onUpdate((param: {theta: number}) => {
-                this.arrangeUnits(param.theta);
+                this.arrangeCards(param.theta);
             })
             .start();
     }
